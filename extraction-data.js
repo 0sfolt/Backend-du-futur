67c327b83d9e2d68fb9512eac7ed3f7bdcc918bd
Arthur <arthur.adnot2@gmail.com>
2024-04-10 10:43:25 +0200

    daily

--- extraction-data.js
+++ extraction-data.js
@@ -1,19 +1,20 @@
 import * as R from "ramda";
 import data from"./dataset/meteo20ans.json" assert { type: 'json' };
-const temp=data.hourly.temperature_2m
-const time=data.hourly.time
+const tempmin=data.daily.temperature_2m_min
+const tempmax=data.daily.temperature_2m_max
+const time=data.daily.time
 
 const diffmin = function(a, b) { return a - b; };
 const diffmax = function(a, b) { return b - a; };
 
 const sortac = R.sort(diffmin);
 const sortdec = R.sort(diffmax);
-const minIndex=R.indexOf(sortac(temp)[0]);
-const maxIndex=R.indexOf(sortdec(temp)[0])
-console.log(minIndex(temp));
-console.log(maxIndex(temp));
-console.log(temp[minIndex(temp)]);
-console.log(time[minIndex(temp)]);
-console.log(temp[maxIndex(temp)]);
-console.log(time[maxIndex(temp)]);
+const minIndex=R.indexOf(sortac(tempmin)[0]);
+const maxIndex=R.indexOf(sortdec(tempmax)[0])
+console.log(minIndex(tempmin));
+console.log(maxIndex(tempmax));
+console.log(tempmin[minIndex(tempmin)]);
+console.log(time[minIndex(tempmin)]);
+console.log(tempmax[maxIndex(tempmax)]);
+console.log(time[maxIndex(tempmax)]);
 
