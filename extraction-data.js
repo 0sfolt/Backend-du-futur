911592d9b089c7410eb7a02957ffcfe6a0492056
Arthur <arthur.adnot2@gmail.com>
2024-05-01 22:51:27 +0200

    Tres nul mais ça marche

--- extraction-data.js
+++ extraction-data.js
@@ -29,17 +29,44 @@ const byMonthOfYear=(t)=>{
 }
 const maxForEach= R.mapObjIndexed(R.reduce(R.max, -Infinity))
 const minForEach= R.mapObjIndexed(R.reduce(R.min, Infinity))
+const meanForEach = R.mapObjIndexed(R.mean)
+const medianForEach = R.mapObjIndexed(R.median)
 
-//console.log(minIndex(tempmin));
-//console.log(maxIndex(tempmax));
-console.log("journée la plus froide des 20 dernières années: " +time[minIndex(tempmin)]+" "+tempmin[minIndex(tempmin)]+"°C");
-console.log("journée la plus chaude des 20 dernières années: " +time[maxIndex(tempmax)]+" "+tempmax[maxIndex(tempmax)]+"°C");
-//console.log(byYear(tempmax))
-//console.log(byMonthOfYear(tempmax))
-console.log(maxForEach(byMonthOfYear(tempmax))) //Température max par mois
-console.log(minForEach(byMonthOfYear(tempmin))) //Température min par mois
-console.log(maxForEach(byYear(tempmax))) //Température max pour chaque années
-console.log(minForEach(byYear(tempmin))) //Température min pour chaque années
+const maxPerMonth = maxForEach(byMonthOfYear(tempmax))
+const minPerMonth = minForEach(byMonthOfYear(tempmin))
+const meanOfMaxPerMonth = meanForEach(byMonthOfYear(tempmax))
+const medianOfMaxPerMonth = medianForEach(byMonthOfYear(tempmax))
+const meanOfMinPerMonth = meanForEach(byMonthOfYear(tempmin))
+const medianOfMinPerMonth = medianForEach(byMonthOfYear(tempmin))
+const maxMois = R.reduce(R.max, -Infinity)
+const minMois = R.reduce(R.min, Infinity)
+function getKeyByValue(object, value) {
+    return Object.keys(object).find(key => object[key] === value);
+}
+
+console.log("Température maximum par mois")
+console.log(maxPerMonth)
+console.log("Température minimum par mois")
+console.log(minPerMonth)
+console.log("Température maximum par année")
+console.log(maxForEach(byYear(tempmax)))
+console.log("Température minimum par année")
+console.log(minForEach(byYear(tempmin)))
+console.log("Température max moyenne par mois")
+console.log(meanOfMaxPerMonth)
+console.log("Température max moyenne par année")
+console.log(meanForEach(byYear(tempmax)))
+console.log("Température max médianne par mois")
+console.log(medianOfMaxPerMonth)
+console.log("Température max médianne par année")
+console.log(medianForEach(byYear(tempmax)))
+
+console.log("journée la plus froide des 20 dernières années: " +time[minIndex(tempmin)]+" avec "+tempmin[minIndex(tempmin)]+"°C");
+console.log("journée la plus chaude des 20 dernières années: " +time[maxIndex(tempmax)]+" avec "+tempmax[maxIndex(tempmax)]+"°C");
+console.log("Mois avec la moyenne de température la plus élevée des 20 dernières années: " +getKeyByValue(meanOfMaxPerMonth,maxMois(Object.values(meanOfMaxPerMonth)))+" avec "+maxMois(Object.values(meanOfMaxPerMonth))+"°C en moyenne");
+console.log("Mois avec la moyenne de température la plus basse des 20 dernières années: " +getKeyByValue(meanOfMinPerMonth,minMois(Object.values(meanOfMinPerMonth)))+" avec "+minMois(Object.values(meanOfMinPerMonth))+"°C en moyenne");
+console.log("Mois avec la médianne de température la plus élevée des 20 dernières années: " +getKeyByValue(medianOfMaxPerMonth,maxMois(Object.values(medianOfMaxPerMonth)))+" avec "+maxMois(Object.values(medianOfMaxPerMonth))+"°C ");
+console.log("Mois avec la médianne de température la plus basse des 20 dernières années: " +getKeyByValue(medianOfMinPerMonth,minMois(Object.values(medianOfMinPerMonth)))+" avec "+minMois(Object.values(medianOfMinPerMonth))+"°C ");
 
 
 
