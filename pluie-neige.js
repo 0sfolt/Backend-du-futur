637a557211ec848e3800987a81c0cdb2c678a0cc
meluxis <melo.desbos@gmail.com>
2024-05-02 19:25:05 +0200

    while doing

--- pluie-neige.js
+++ pluie-neige.js
@@ -45,14 +45,13 @@ function dayDateExpress (valeur){
     return `Le ${day} ${monthNames[month]}, ${year} `
 }
 //SNOW///////////////////////////////////////////////////////////////////////////////////////////////
-//Journee plus de neige
+////Journee plus de neige
 //Trouver le max dans une liste et recuperer le jour associé
 var dayMaxAmountSnow = R.reduce(R.max, -Infinity,snowfallSum);
 var dayMaxSnowIndex= R.indexOf(dayMaxAmountSnow,snowfallSum );
 var dayMaxSnow =time[dayMaxSnowIndex];
-console.log(`Journée plus enneigée sur 20ans: ${dayDateExpress(dayMaxSnow)}  avec ${dayMaxAmountSnow} cm de neige`);
 
-//Mois plus de neige
+////3 Mois plus de neige
 //Somme de neige par mois
 const SumPerMonth = R.mapObjIndexed(R.sum);
 const SumSnowPerMonth = SumPerMonth(byMonthOfYear(snowfallSum));
@@ -65,19 +64,35 @@ const sortedBySnowfall = R.sortWith(
 // Recup les trois premiers
 const topThreeSnowfall = R.take(3, sortedBySnowfall);
 
-console.log(`Top 3 des mois les plus enneigé: 
-            ${monthDateExpress(topThreeSnowfall[0][0])} avec ${topThreeSnowfall[0][1]} cm de neige,
-            ${monthDateExpress(topThreeSnowfall[1][0])} avec ${topThreeSnowfall[1][1]} cm de neige,
-            ${monthDateExpress(topThreeSnowfall[2][0])} avec ${topThreeSnowfall[2][1]} cm de neige`);
+////Annee plus enneigée
+//Somme de neige par mois
+const SumPerYear = R.mapObjIndexed(R.sum);
+const SumSnowPerYear = SumPerYear(byYear(snowfallSum));
+
+//Trie liste
+const sortedBySnowfallY = R.sortWith(
+    [R.descend(R.last)],
+    R.toPairs(SumSnowPerYear),
+);
+
+////Pourcentage
+//nbr de jour de pluie sur 20ans
+console.log(snowfallSum)
+let numSnowDays = 0;
+const fn = R.cond([
+    [R.equals(0),   R.always(numSnowDays+=1)],
+    [R.equals(100), R.always('water boils at 100°C')],
+    [R.T,           temp => 'nothing special happens at ' + temp + '°C']
+]);
+//Il a plu tata % du temps
 
 //PLUIE///////////////////////////////////////////////////////////////////////////////////////////////
-// Journée plus de pluie
+//// Journée plus de pluie
 var dayMaxAmountRain = R.reduce(R.max, -Infinity,rainSum);
 var dayMaxRainIndex= R.indexOf(dayMaxAmountRain,rainSum );
 var dayMaxRain =time[dayMaxRainIndex];
-console.log(`Journée la plus pluvieuse: ${dayDateExpress(dayMaxRain)} avec ${dayMaxAmountRain} mm de pluie`);
 
-//Mois plus de pluie
+////Top 3 Mois plus de pluie
 //Somme de pluie par mois
 const SumRainPerMonth = SumPerMonth(byMonthOfYear(rainSum));
 
@@ -89,9 +104,32 @@ const sortedByRainfall = R.sortWith(
 // Recup les trois premiers
 const topThreeRainfall = R.take(3, sortedByRainfall);
 
+////Annee plus pluvieuse
+//Somme de neige par mois
+const SumRainPerYear = SumPerYear(byYear(rainSum));
+
+//Trie liste
+const sortedByRainfallY = R.sortWith(
+    [R.descend(R.last)],
+    R.toPairs(SumRainPerYear),
+);
+
+console.log(`Journée la plus pluvieuse: ${dayDateExpress(dayMaxRain)} avec ${dayMaxAmountRain} mm de pluie`);
 console.log(`Top 3 des mois les plus pluvieux: 
             ${monthDateExpress(topThreeRainfall[0][0])} avec ${topThreeRainfall[0][1]} mm de pluie,
             ${monthDateExpress(topThreeRainfall[1][0])} avec ${topThreeRainfall[1][1]} mm de pluie,
-            ${monthDateExpress(topThreeRainfall[2][0])} avec ${topThreeRainfall[2][1]} mm de pluie`);
+            ${monthDateExpress(topThreeRainfall[2][0])} avec ${topThreeRainfall[2][1]} mm de pluie \n`);
+console.log(`L'année la plus pluvieuse: 
+            L'année ${sortedByRainfallY[0][0]} avec ${sortedByRainfallY[0][1]} mm de pluie`);
+console.log(`L'année la moins pluvieuse: 
+            L'année ${sortedByRainfallY[20][0]} avec ${sortedByRainfallY[20][1]} mm de pluie`);
 
-
+console.log(`Journée plus enneigée sur 20ans: ${dayDateExpress(dayMaxSnow)}  avec ${dayMaxAmountSnow} cm de neige`);
+console.log(`Top 3 des mois les plus enneigés: 
+            ${monthDateExpress(topThreeSnowfall[0][0])} avec ${topThreeSnowfall[0][1]} cm de neige,
+            ${monthDateExpress(topThreeSnowfall[1][0])} avec ${topThreeSnowfall[1][1]} cm de neige,
+            ${monthDateExpress(topThreeSnowfall[2][0])} avec ${topThreeSnowfall[2][1]} cm de neige`);
+console.log(`L'année la plus enneigée: 
+            L'année ${sortedBySnowfallY[0][0]} avec ${sortedBySnowfallY[0][1]} cm de neige`);
+console.log(`L'année la moins enneigée: 
+            L'année ${sortedBySnowfallY[20][0]} avec ${sortedBySnowfallY[20][1]} cm de neige`);
